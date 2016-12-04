e5f501bb32575afa4e2297a5edfae1926e2098b9
Alfred-Garcia <alfredgarcia10@gmail.com>
2016-12-04 14:09:59 -0800

    Add files via upload

---
+++ newJavaScript.js
@@ -0,0 +1,11 @@
+function revealVideo() {
+    document.getElementById("video").style.visibility="visible";
+}
+
+function revealPlaystation() {
+    document.getElementById("playstation").style.visibility="visible";
+}
+
+function revealNintendo() {
+    document.getElementById("nintendo").style.visibility="visible";
+}
